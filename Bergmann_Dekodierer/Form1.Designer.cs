a0e2405446c98a697b2fb910e802690567654fec
Benjamin Bergmann <89748204+steilgedacht@users.noreply.github.com>
2021-12-31 13:27:57 +0100

    Add files via upload

---
+++ Bergmann_Dekodierer/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Bergmann_Dekodierer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(289, 269);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Text";
+            this.richTextBox1.Click += new System.EventHandler(this.richTextBox1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.richTextBox2.Location = new System.Drawing.Point(289, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(195, 461);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "Result";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripComboBox1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 436);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(289, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(51, 22);
+            this.toolStripButton1.Text = "Decode";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Full Result Mode",
+            "Missing Words Mode"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(0, 275);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "Missing Words";
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.YellowGreen;
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(40, 22);
+            this.toolStripButton2.Text = "Color";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bergmann Dekodierer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Bergmann_Dekodierer/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Bergmann_Dekodierer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] dateiinhalt;
+        string Output;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] Dateiihnahlt = File.ReadAllLines(@"Vokabelliste\Vokabelliste.txt");
+            dateiinhalt = Dateiihnahlt;
+            Scalingsize();
+            toolStripComboBox1.SelectedIndex = 0;
+        }
+
+        private void Scalingsize()
+        {
+            richTextBox1.Width = Convert.ToInt16(Math.Round(Convert.ToDouble(this.Size.Width) / 2 - 20, 0));
+            richTextBox2.Width = Convert.ToInt16(Math.Round(Convert.ToDouble(this.Size.Width) / 2, 0));
+            richTextBox3.Width = richTextBox1.Width;
+            richTextBox1.Height = richTextBox2.Height - toolStrip1.Height - 100;
+            richTextBox3.Height = 100;
+            richTextBox3.Location = new Point(0, richTextBox1.Height);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox1.Text; //text vorbereiten
+            text = text.Replace(@"\", @" \ ");
+            text = text.Replace(@"-", @" - ");
+            text = text.Replace(@".", @" . ");
+            text = text.Replace(@",", @" , ");
+            text = text.Replace(@";", @" ; ");
+            text = text.Replace(@":", @" : ");
+            text = text.Replace(@"(", @" ( ");
+            text = text.Replace(@")", @" ) ");
+            text = text.Replace(@"!", @" ! ");
+            text = text.Replace(@"]", @" ] ");
+            text = text.Replace(@"[", @" [ ");
+            text = text.Replace(@"/", @" / ");
+            text = text.Replace(@"&", @" & ");
+            text = text.Replace(@"+", @" + ");
+            text = text.Replace(@"@", @" @ ");
+            text = text.Replace(@"<", @" < ");
+            text = text.Replace(@">", @" > ");
+            text = text.ToLower();
+            text = text.Replace(@" i ", @" I ");
+            text = text.Replace(@"  ", @" ");
+            richTextBox1.Text = text;
+
+            switch (toolStripComboBox1.SelectedIndex)
+            {
+                case 0:
+                    FullResultMode();
+                    break;
+                case 1:
+                    MissingWordMode();
+                    break;
+            }
+        }
+
+        private void FullResultMode()
+        {
+            string[] englischeWörter = richTextBox1.Text.Split(" ".ToCharArray());
+            string[] deutscheWörter = new string[englischeWörter.Length];
+
+            for (int j = 0; j < englischeWörter.GetLength(0); j++)
+            {
+                for (int i = 0; i < dateiinhalt.GetLength(0); i++)
+                {
+                    if ((englischeWörter[j].Equals(dateiinhalt[i]) || englischeWörter[j].Equals(dateiinhalt[i] + ","))|| (englischeWörter[j].Equals(dateiinhalt[i] + ".") || englischeWörter[j].Equals(dateiinhalt[i] + ":")))
+                    {
+                        deutscheWörter[j] = (dateiinhalt[i + 1] + " ");
+                        break;
+                    }
+                    else
+                    {
+                        deutscheWörter[j] = " ";
+                    }
+                }
+                if (deutscheWörter[j] == " ")
+                {
+                    deutscheWörter[j] = englischeWörter[j];
+                }
+            }
+
+
+            Form2 newForm2 = new Form2();
+
+            int locx = 1, locy = 1;
+            double Weite, Multiplikator = 3, Power = 100;
+            for (int a = 0; a < englischeWörter.Length; a++)
+            {
+                Weite = 0;
+                try //Die Weite auswählen je nachdem wie lang der String ist wird die Länge des Labels gewählt
+                {
+                    if (englischeWörter[a].Length >= deutscheWörter[a].Length)
+                    {
+                        Weite = Multiplikator * Math.Sqrt(englischeWörter[a].Length* Power) + 10;
+                    }
+                    else
+                    {
+                        Weite = Multiplikator * Math.Sqrt(deutscheWörter[a].Length * Power) + 10;
+                    }
+                }
+                catch
+                {
+                    Weite = 0;
+                    Weite = Multiplikator * Math.Sqrt(englischeWörter[a].Length * Power) + 10;
+                }
+                if ((englischeWörter[a] == ","|| englischeWörter[a] == "/") ||(englischeWörter[a] == "."|| englischeWörter[a] == "-"))
+                {
+                    Weite = 20;
+                }
+
+
+                Label labl = addlabel(a, englischeWörter[a], locx, locy, true, Convert.ToInt16(Math.Round(Weite, 0)));
+                Label labl2 = addlabel(a, deutscheWörter[a], locx, locy + 20, false, Convert.ToInt16 (Math.Round(Weite,0)));
+                newForm2.Controls.Add(labl);
+                newForm2.Controls.Add(labl2);
+                if (locx >= newForm2.Right - 200)
+                {
+                    locx = 5;
+                    locy += 50;
+                }
+                else
+                {
+                    locx += Convert.ToInt16(Math.Round(Weite,0));
+                }
+            }
+            newForm2.Height = locy + 80;
+            newForm2.ShowDialog();
+        }
+
+
+
+        Label addlabel(int i, string text,int startx, int endx, bool fett, int Waite)
+        {
+            Label labl = new Label();
+            labl.Name = "Label" + i.ToString();
+            labl.Text = text;
+            labl.BackColor = Color.WhiteSmoke;
+            if (fett == true)
+            {
+                labl.Font = new Font("Verdana", 11, FontStyle.Bold);
+                labl.TextAlign = ContentAlignment.BottomLeft;
+            }
+            else
+            {
+                labl.Font = new Font("Verdana", 11, FontStyle.Regular);
+                labl.TextAlign = ContentAlignment.TopLeft;
+            }
+            labl.Click += new EventHandler(this.Labl_Click); //Click Ereigniss
+            labl.DoubleClick += new EventHandler(this.Labl_DoubleClick); //Doppeclick Ereigniss
+            labl.Height = 20;
+            labl.Width = Waite;
+            labl.BringToFront();
+            labl.Visible = true;
+            labl.Location = new Point(startx, endx);
+            return labl;
+            
+        }
+
+        private void Labl_DoubleClick(object sender, EventArgs e)
+        {
+        }
+
+        protected void Labl_Click(object sender, EventArgs e)  //Click Ereigniss eines Labels, hier wird die Farbe geändert
+        {
+            Label clickedLabel = (Label)sender;
+            if (clickedLabel.BackColor == colorDialog1.Color)
+            {
+                clickedLabel.BackColor = Color.WhiteSmoke;
+            }
+            else
+            {
+                clickedLabel.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void MissingWordMode()
+        {
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            Output = "";
+            string[] wörter = richTextBox1.Text.Split(" ".ToCharArray());
+            string nichtVorhanden = "";
+            bool nichtvorhandenabfrage = true;
+
+            for (int j = 0; j < wörter.GetLength(0); j++)
+            {
+                nichtvorhandenabfrage = true;
+                for (int i = 0; i < dateiinhalt.GetLength(0); i++)
+                {
+                    if ((wörter[j].Equals(dateiinhalt[i]) || wörter[j].Equals(dateiinhalt[i] + ",")) || (wörter[j].Equals(dateiinhalt[i] + ".") || wörter[j].Equals(dateiinhalt[i] + ":")))
+                    {
+                        Output += dateiinhalt[i + 1] + " ";
+                        nichtvorhandenabfrage = false;
+                        break;
+                    }
+
+                }
+                if (nichtvorhandenabfrage == true)
+                {
+                    nichtVorhanden += wörter[j] + " ";
+                }
+
+            }
+
+            richTextBox2.Text = Output;
+            richTextBox3.Text = nichtVorhanden;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Scalingsize();
+        }
+
+        private void richTextBox1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "Text")
+            {
+                richTextBox1.Clear();
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+        }
+    }
+}
